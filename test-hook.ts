b79a31d9e6b73f40dffb3f4c8099851334be28df
SebastianZarate <juzaor2003@gmail.com>
2026-03-03 22:58:52 -0500

    chore: eliminar archivo de prueba

--- test-hook.ts
+++
@@ -1,9 +0,0 @@
-// Test file para verificar pre-commit hook
-const test = 'mal formateado';
-const otro = 'sin espacios';
-
-function testFunction() {
-  console.log(test, otro);
-}
-
-export { testFunction };
