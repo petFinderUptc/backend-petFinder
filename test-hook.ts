894ec68cdd262c1eecd28f37957170b3a315e81d
SebastianZarate <juzaor2003@gmail.com>
2026-03-03 22:56:58 -0500

    test: verificar pre-commit hook

---
+++ test-hook.ts
@@ -0,0 +1,9 @@
+// Test file para verificar pre-commit hook
+const test = 'mal formateado';
+const otro = 'sin espacios';
+
+function testFunction() {
+  console.log(test, otro);
+}
+
+export { testFunction };
